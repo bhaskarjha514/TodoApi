b22d45f33fa02394af9390b6cdc33d245aeb57bb
bhaskarjha514 <bhaskarjha514@gmail.com>
2020-08-31 07:20:04 +0530

    fixed bug

--- routes/user.js
+++ routes/user.js
@@ -12,36 +12,35 @@ router.post('/register',async (req, res, next)=>{
     let code = generateOTP()
     try{
         let user_exist = await User.findOne({'email':email});
+       
         if(user_exist){
-            console.log(user_exist)
             res.status(404).json({
-                
                 msg:"Email Already used"
             })
-        }
-
-    user = new User()
-    user.username = username
-    user.email = email
-    const salt = await bcryptjs.genSalt(10)
-    user.password = await bcryptjs.hash(password, salt)
-
-    const authData = new Auth({
-        email,
-        code
-    })
+        }else{
+            user = new User()
+            user.username = username
+            user.email = email
+            const salt = await bcryptjs.genSalt(10)
+            user.password = await bcryptjs.hash(password, salt)
         
-    msg = mailer.mailer(email, code)
-    if(msg){
-        return res.status(400).json({'message' : 'Cannot Send activation mail ! Check your email and try again'})
-    }
-    await user.save(async (err, response) => {
-        if (err) res.status(500).json({ 'message': 'Server Fucked Up 😑😢' })
-        await authData.save((err, response) => {
-            if (err) res.status(500).json({ 'message': 'Server Fucked Up 😑😢' })
-                res.status(201).json({ 'message': 'SignUp successful', 'User': user })
-            })
-       })
+            const authData = new Auth({
+                email,
+                code
+            }) 
+                
+            msg = mailer.mailer(email, code)
+            if(msg){
+                return res.status(400).json({'message' : 'Cannot Send activation mail ! Check your email and try again'})
+            }
+            await user.save(async (err, response) => {
+                if (err) res.status(500).json({ 'message': 'Server Fucked Up 😑😢' })
+                await authData.save((err, response) => { 
+                    if (err) res.status(500).json({ 'message': 'Server Fucked Up 😑😢' })
+                        res.status(201).json({ 'message': 'SignUp successful', 'User': user })
+                    })
+               })
+        }
 
     }catch(err){
         console.log(err)
