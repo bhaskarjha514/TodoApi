f16165fbd2068f7bccd7356c8004332021b274c2
Bhaskar Jha <35396885+bhaskarjha514@users.noreply.github.com>
2020-09-07 15:09:17 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -16,9 +16,13 @@ router.post('/register',async (req, res, next)=>{
         if(user_exist){
             let checkAuth = await Auth.findOne({'email':email});
             if(checkAuth){
-                res.status(404).send()
+                res.status(404).json({
+                    msg:"Email Already used! Enter Otp"
+                }) 
             }else{
-                res.status(404).send()
+                res.status(404).json({
+                    msg:"Email Already used"
+                })
             }
            
         }else{
@@ -35,12 +39,12 @@ router.post('/register',async (req, res, next)=>{
                 
             msg = mailer.mailer(email, code)
             if(msg){
-                return res.status(400).send()
+                return res.status(400).json({'message' : 'Cannot Send activation mail ! Check your email and try again'})
             }
             await user.save(async (err, response) => {
-                if (err) return res.status(400).send()
+                if (err) return res.status(400).json({ 'message': 'Server Fucked Up 😑😢' })
                 await authData.save((err, response) => { 
-                    if (err) return res.status(400).send()
+                    if (err) return res.status(400).json({'message':'server fucked up'})
                         return res.status(201).json({'message':'otp has been send to email'})
                     })
                })
@@ -48,7 +52,10 @@ router.post('/register',async (req, res, next)=>{
 
     }catch(err){
         console.log(err)
-        res.status(500).send()
+        res.status(500).json({
+            success: false,
+            msg:'server error'
+        })
     }    
 })
 
@@ -60,11 +67,11 @@ router.get('/verify/', async (req, res) => {
     const acc = await Auth.findOne({'email':email})
    
     if(!(acc.code == code)){
-        return res.status(400).send()
+        return res.status(400).json({msg:'otp is incorrect'})
     }else{
         await Auth.findOneAndDelete({'email':email})
         await User.findOneAndUpdate({'email':email}, {'isVerified':true})
-        return res.status(201).send()
+        return res.status(201).json({'message' : 'Account has verified'})
     }
 })
 
@@ -75,23 +82,29 @@ router.post('/login', async (req, res, next)=>{
     try{
         let user = await User.findOne({'email':email})
         if(!user){
-            res.status(400).send()
+            res.status(400).json({
+                success: false,
+                msg: "Email not Registered"
+            })
         }else{
             const isVerified = await user.isVerified
             if(!isVerified){
-                res.status(400).send()
+                res.status(400).json({success:false, msg:'Email is not verified'})
             }else{
                 const isMatch = await bcryptjs.compare(password,user.password)
                 if(!isMatch){
-                    return res.status(400).send()
+                    return res.status(400).json({success:false, msg:'wrong password'})
                 }else{
-                    return res.status(201).send()
+                    return res.status(201).json({success:true, msg:'successfully login'})
                 }
             }
         }
     }catch(err){
         console.log(err)
-        res.status(500).send()
+        res.status(500).json({
+            success: false,
+            msg:'server error'
+        })
     }
 })
 
